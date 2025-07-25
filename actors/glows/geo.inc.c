1020faca2d4f43376b8c308d31e1f4609d224621
winbowbreaker <mikeandcbanimations@gmail.com>
2025-07-25 18:12:58 +0100

    filesize reduction
    yayyy

--- actors/glows/geo.inc.c
+++
@@ -1,9 +0,0 @@
-#include "src/game/envfx_snow.h"
-
-const GeoLayout glows_geo[] = {
-	GEO_NODE_START(),
-	GEO_OPEN_NODE(),
-		GEO_BILLBOARD_WITH_PARAMS_AND_DL(LAYER_TRANSPARENT, 0, 0, 0, glows_000_displaylist_mesh_layer_5),
-	GEO_CLOSE_NODE(),
-	GEO_END(),
-};
--- actors/glows/geo_header.h
+++
@@ -1,8 +0,0 @@
-extern const GeoLayout glows_geo[];
-extern Lights1 glows_Fast3D_Material_lights;
-extern u8 glows_glowystuff_rgba16[];
-extern Vtx glows_000_displaylist_mesh_layer_5_vtx_0[4];
-extern Gfx glows_000_displaylist_mesh_layer_5_tri_0[];
-extern Gfx mat_glows_Fast3D_Material[];
-extern Gfx mat_revert_glows_Fast3D_Material[];
-extern Gfx glows_000_displaylist_mesh_layer_5[];
